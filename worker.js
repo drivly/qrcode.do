532392f611715ab6e353da524bd866c9652576c0
Nathan Clevenger <nateclev@gmail.com>
2023-03-22 02:16:20 -0500

    Initial commit

---
+++ worker.js
@@ -0,0 +1,51 @@
+export const api = {
+  icon: '🚀',
+  name: 'templates.do',
+  description: 'Cloudflare Worker Template',
+  url: 'https://templates.do/api',
+  type: 'https://apis.do/templates',
+  endpoints: {
+    listCategories: 'https://templates.do/api',
+    getCategory: 'https://templates.do/:type',
+  },
+  site: 'https://templates.do',
+  login: 'https://templates.do/login',
+  signup: 'https://templates.do/signup',
+  subscribe: 'https://templates.do/subscribe',
+  repo: 'https://github.com/drivly/templates.do',
+}
+
+export const gettingStarted = [
+  `If you don't already have a JSON Viewer Browser Extension, get that first:`,
+  `https://extensions.do`,
+]
+
+export const examples = {
+  listItems: 'https://templates.do/worker',
+}
+
+export default {
+  fetch: async (req, env) => {
+    const { user, hostname, pathname, rootPath, pathSegments, query } = await env.CTX.fetch(req).then(res => res.json())
+    if (rootPath) return json({ api, gettingStarted, examples, user })
+    
+    // TODO: Implement this
+    const [ resource, id ] = pathSegments
+    const data = { resource, id, hello: user.city }
+    
+    return json({ api, data, user })
+  }
+}
+
+const json = (obj, init) => {
+  if (!init) {
+    init = {}
+  }
+  if (!init.headers) {
+    init.headers = {}
+  }
+  if (!init.headers['Content-Type'] && !init.headers['content-type']) {
+    init.headers['Content-Type'] = 'application/json; charset=utf-8'
+  }
+  return new Response(JSON.stringify(obj, null, 2), init)
+}

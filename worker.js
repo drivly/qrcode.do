751405c0d53a04edb2ca01e1ae71a2080b3f465c
Nathan Clevenger <nateclev@gmail.com>
2023-03-22 02:40:45 -0500

    Update worker.js

--- worker.js
+++ worker.js
@@ -5,9 +5,9 @@ export default {
     const { user, hostname, pathname, rootPath, pathSegments, query } = await env.CTX.fetch(req).then(res => res.json())
 
     const qrcode = await QRCode.create('https:/' + pathname)
-    const svg = await qrcode.toString('https:/' + pathname, { type: 'svg' })
-    const utf = await qrcode.toString('https:/' + pathname, { type: 'utf8' })
-    const terminal = await qrcode.toString('https:/' + pathname, { type: 'terminal' })
+    const svg = await QRCode.toString('https:/' + pathname, { type: 'svg' })
+    const utf = await QRCode.toString('https:/' + pathname, { type: 'utf8' })
+    const terminal = await QRCode.toString('https:/' + pathname, { type: 'terminal' })
     
     return new Response(JSON.stringify({qrcode, svg, utf, terminal}), {})
   }
